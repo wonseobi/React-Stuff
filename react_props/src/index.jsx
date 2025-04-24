9403da75d14a11103b67757c35426bcf33ec3900
Won Lee <wonseob2207@gmail.com>
2025-04-24 12:23:01 -0600

    Add files via upload

---
+++ react_props/src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      tel="+12321432"
+      email="jackbauer@email.com"
+    />
+    <input id="fName" />
+  </div>,
+
+  document.getElementById("root")
+);
