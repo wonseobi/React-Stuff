4cac1501837956a3dd7c1be841a264b4f9c90fd6
Won Lee <wonseob2207@gmail.com>
2025-07-01 15:04:36 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description} 
+            image={CORE_CONCEPTS[0].image} />
+          <CoreConcept 
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description} 
+            image={CORE_CONCEPTS[1].image} />
+          <CoreConcept 
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description} 
+            image={CORE_CONCEPTS[2].image} />
+          <CoreConcept 
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description} 
+            image={CORE_CONCEPTS[3].image} />
+        </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+              <TabButton>Components</TabButton>
+
+          </menu>
+        </section>
+
       </main>
     </div>
   );
